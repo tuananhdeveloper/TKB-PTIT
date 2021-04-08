872ef6b761b2325896cede29b6d04fc85d6ee40b
Nguyễn Tuấn Anh <47476675+tuananhdeveloper@users.noreply.github.com>
2021-04-08 21:13:27 +0700

    Update MyServlet.java

--- src/servlet/MyServlet.java
+++ src/servlet/MyServlet.java
@@ -36,6 +36,29 @@ public class MyServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String studentCode = request.getParameter("code");
+		String token = request.getParameter("downloadToken");
+		
+		ICSExporter exporter = new ICSExporter(studentCode, token);
+
+		
+		boolean result = false;
+		try {
+
+			result = exporter.export(response);
+			if(result == false) {
+				response.setContentType("text/html; charset=UTF-8");
+				response.setCharacterEncoding("UTF-8");
+				OutputStream out = response.getOutputStream();
+				OutputStreamWriter osw = new OutputStreamWriter(out);
+				osw.write("Mã sinh viên không đúng");
+				osw.close();
+				out.close();
+			}
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
